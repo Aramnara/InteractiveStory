96fdd35fdc071d2c7b52c7442e91bf9add3874ad
Anthony <anthonyramnarain7@gmail.com>
2018-11-12 14:36:03 -0500

    Initial commit

---
+++ app/src/main/java/com/anthonyramnarain/interactivestory/MainActivity.java
@@ -0,0 +1,37 @@
+package com.anthonyramnarain.interactivestory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button StartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = (EditText) findViewById(R.id.nameEditText);
+        StartButton = (Button) findViewById(R.id.StartButton);
+
+        // Anonymous inner class
+        StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                startStory();
+            }
+        });
+    }
+
+    private void startStory() {
+        // Intent
+
+    }
+}
